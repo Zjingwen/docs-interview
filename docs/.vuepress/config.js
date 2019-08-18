fdd09afc3eeb377dbbbf1592b5cebcf00f207c3f
静文（jingwen） <jingwen@xiaoshijie.com>
2019-08-18 17:07:49 +0800

    feature: 修改文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,50 +1,24 @@
 module.exports = {
+  base: '/doc/',
   locales: {
     '/': {
       lang: 'zh-CN',
-      title: '前端面试与进阶指南',
-      description: '可能是全网最给力的前端面试项目'
-    }
+      title: '伪公知的生活常态',
+      description: '就是随便写写而已',
+    },
   },
   themeConfig: {
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'External', link: 'https://google.com' },
-    ],
     sidebar: [
       {
         title: '个人笔记',
         collapsable: false,
-        path: '/个人笔记/',
-        children:[
-          '/'
-        ]
+        path: '/个人笔记/'
       },
       {
         title: '收藏笔记',
-        collapsable: true,
-        path: '/收藏笔记/',
-        children:[
-          '/'
-        ]
-      },
-      // {
-      //   title: '知乎收藏',
-      //   collapsable: true,
-      //   path: '/知乎收藏/',
-      //   children:[
-      //     '/知乎收藏/'
-      //   ]
-      // },
-      {
-        title: '民国手札',
-        collapsable: true,
-        path: '/民国手札/',
-        children:[
-          '/'
-        ]
-      },
+        collapsable: false,
+        path: '/收藏笔记/'
+      }
     ]
   }
 }
