93442f2744da06c816ae599f51dab30376d6bca5
静文（jingwen） <jingwen@xiaoshijie.com>
2019-09-02 14:41:18 +0800

    release: 调整大纲

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,63 @@ module.exports = {
         title: '面试题',
         collapsable: false,
         path: '/question/'
+      },
+      {
+        title: 'ecmascript'
+      },
+      {
+        title: 'vue'
+      },
+      {
+        title: 'react'
+      },
+      {
+        title: 'typescript'
+      },
+      {
+        title: '浏览器'
+      },
+      {
+        title: 'css'
+      },
+      {
+        title: 'git'
+      },
+      {
+        title: 'rxjs'
+      },
+      {
+        title: 'webpack'
+      },
+      {
+        title: 'http'
+      },
+      {
+        title: 'node'
+      },
+      {
+        title: '微信小程序'
+      },
+      {
+        title: '算法'
+      },
+      {
+        title: '统计'
+      },
+      {
+        title: '测试'
+      },
+      {
+        title: '发布/部署'
+      },
+      {
+        title: '前端优化'
+      },
+      {
+        title: '团队管理'
+      },
+      {
+        title: '设计模式'
       }
     ]
   }
