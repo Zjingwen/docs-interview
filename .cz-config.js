04c888ebddd9098326d3922b988c2e9a00d3b6b0
静文（jingwen） <jingwen@xiaoshijie.com>
2019-09-04 00:37:11 +0800

    chore: 调试.cz-config.js文件

--- .cz-config.js
+++ .cz-config.js
@@ -4,40 +4,42 @@ module.exports = {
   types: [
     {
       value: 'WIP',
-      name : '💪  WIP:      正在开发中'
+      name: '💪  WIP:      正在开发中'
     },
     {
       value: 'feat',
-      name : '✨  feat:     新功能'
+      name: '✨  feat:     新功能'
     },
     {
       value: 'fix',
-      name : '🐞  fix:      修复，错误修复'
+      name: '🐞  fix:      修复，错误修复'
     },
     {
       value: 'refactor',
-      name : '🛠  refactor: 重构，代码更改既不修复错误也不添加功能'
+      name: '🛠  refactor: 重构，代码更改既不修复错误也不添加功能'
     },
     {
       value: 'docs',
-      name : '📚  docs:     修改文档'
+      name: '📚  docs:     修改文档'
     },
     {
       value: 'test',
-      name : '🏁  test:     添加缺失测试或更正现有测试'
+      name: '🏁  test:     添加缺失测试或更正现有测试'
     },
     {
       value: 'chore',
-      name : '🗯  chore:    不修改src或测试文件的更改。比如更新构建任务，包管理器'
+      name: '🗯  chore:    不修改src或测试文件的更改。比如更新构建任务，包管理器'
     },
     {
       value: 'style',
-      name : '💅  style:    代码风格，不影响代码含义的更改（空格，格式，缺少分号等）'
+      name: '💅  style:    代码风格，不影响代码含义的更改（空格，格式，缺少分号等）'
     },
     {
       value: 'revert',
-      name : '⏪  revert:   恢复为提交 '
+      name: '⏪  revert:   恢复为提交 '
     }
   ],
-  allowBreakingChanges: ["feat", "fix"]
+  skipQuestions: ['scope', 'body', 'footer'],
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat ', 'fix']
 };
